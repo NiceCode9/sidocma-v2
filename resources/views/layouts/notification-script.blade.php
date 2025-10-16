7c2af1b1224c76756d32edbf210469e7381a6aab
NiceCode9 <nicecode9@gmail.com>
2025-10-16 02:09:25 +0000

    penambahan url view file di pop up notification

--- resources/views/layouts/notification-script.blade.php
+++ resources/views/layouts/notification-script.blade.php
@@ -202,8 +202,8 @@
         }
 
         const notificationHtml = notifications.map(notification => `
-            <a href="#" class="dropdown-item ${!notification.is_read ? 'notification-item-unread' : ''}"
-               onclick="suratNotificationBell.handleNotificationClick('${notification.id}', event)">
+            <a href="${notification.action_url}" class="dropdown-item ${!notification.is_read ? 'notification-item-unread' : ''}"
+               onclick="suratNotificationBell.handleNotificationClick('${notification.id}')">
                 <div class="dropdown-item-icon bg-primary text-white">
                     <i class="fas fa-envelope"></i>
                 </div>
